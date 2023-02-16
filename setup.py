e0b61f99c201a42e3cf5ed22e953b65ca0ae1377
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-15 19:46:02 -0800

    Bump version: 0.8.1 → 0.9.0

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/alifedata-phyloinformatics-convert',
-    version='0.8.1',
+    version='0.9.0',
     zip_safe=False,
 )
