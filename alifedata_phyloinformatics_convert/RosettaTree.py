2f29c8e8fd81ec09df73f042ba55b1f03e5d62c0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-19 10:06:57 +0000

    Fixup docstrings, type hints

--- alifedata_phyloinformatics_convert/RosettaTree.py
+++ alifedata_phyloinformatics_convert/RosettaTree.py
@@ -21,20 +21,17 @@ class RosettaTree:
     Enables user to translate across tree implementations without repeatedly calling
     the individual conversion functions. This allows users to accept any such tree
     as an argument in a library-agnostic way.
-
-    Attributes
-    ----------
-    tree:
-        Tree to convert from. Can be any of the three supported by this library.
     """
+
+    _tree: pandas.DataFrame
+
     def __init__(self, tree: typing.Union[
-            dendropy.Tree,
-            pandas.DataFrame,
-            Bio.Phylo.BaseTree.Tree
-        ]):
-        """Construct tree from dendropy tree, alife standard dataframe, or biopython tree.
-        """
-        # convert any supported tree format to ALife format,
+        dendropy.Tree,
+        pandas.DataFrame,
+        Bio.Phylo.BaseTree.Tree
+    ]) -> None:
+        """Set up underlying data to be viewed, from any supported format."""
+         # convert any supported tree format to ALife format,
         # as this is our interal representation
         if isinstance(tree, dendropy.Tree):
             # is a Dendropy Tree
@@ -50,21 +47,18 @@ class RosettaTree:
 
     @property
     @lru_cache(maxsize=None)
-    def as_biopython(self):
-        """Return stored tree as a biopython tree.
-        """
+    def as_biopython(self: "RosettaTree") -> Bio.Phylo.BaseTree.Tree:
+        """Return stored tree as a BioPython tree."""
         return alife_dataframe_to_biopython_tree(self._tree, setup_edge_lengths=True)
 
     @property
     @lru_cache(maxsize=None)
-    def as_dendropy(self):
-        """Return stored tree as a dendropy tree.
-        """
+    def as_dendropy(self: "RosettaTree") -> dendropy.Tree:
+        """Return stored tree as a DendroPy tree."""
         return alife_dataframe_to_dendropy_tree(self._tree, setup_edge_lengths=True)
 
     @property
     @lru_cache(maxsize=None)
-    def as_alife(self):
-        """Return stored tree as an alife-standardized phylogeny pandas dataframe.
-        """
+    def as_alife(self: "RosettaTree") -> pandas.DataFrame:
+        """Return stored tree as a dataframe in alife standard format."""
         return self._tree
