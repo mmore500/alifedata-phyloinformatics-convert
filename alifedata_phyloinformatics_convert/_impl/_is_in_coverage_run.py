303dae9cf6bd859ca49abd5b7b1e70a0bc27e551
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-19 13:57:18 -0700

    Delete unused file:

--- alifedata_phyloinformatics_convert/_impl/_is_in_coverage_run.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-# adapted from https://stackoverflow.com/a/69531314
-# alternate approach: https://stackoverflow.com/a/69531314
-def is_in_coverage_run() -> bool:
-    """Is code coverage currently being measured?"""
-    return "coverage" in sys.modules
