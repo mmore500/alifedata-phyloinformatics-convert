175b4d9335bc66f07908fc6139987d07bf326930
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-19 14:05:21 -0700

    Fixup pasteover code attribution comments

--- alifedata_phyloinformatics_convert/_impl/_try_alifestd_validate.py
+++ alifedata_phyloinformatics_convert/_impl/_try_alifestd_validate.py
@@ -1,7 +1,7 @@
-"""This file has been copied from hstrat (https://github.com/mmore500/hstrat/blob/47fe9048d6b327ca40f77424e3cf9392f7980689/hstrat/_auxiliary_lib/_alifestd_validate.py).
-It is up to date as of commit 1ce7a0e.
-Eventually, it will be migrated over entirely.
-"""
+# This file has been copied from hstrat (https://github.com/mmore500/hstrat/blob/47fe9048d6b327ca40f77424e3cf9392f7980689/hstrat/_auxiliary_lib/_alifestd_validate.py).
+# It is up to date as of commit
+# [1ce7a0e](https://github.com/mmore500/hstrat/commit/1ce7a0e).
+# Eventually, it should be migrated to a standalone library.
 
 import pandas as pd
 
