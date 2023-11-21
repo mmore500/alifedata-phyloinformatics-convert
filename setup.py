e9cf6ac94f8ffaf4fef78da0300ac8864a5cde05
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-20 19:03:06 -0500

    Note support for newer python versions

--- setup.py
+++ setup.py
@@ -40,6 +40,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     description="alifedata-phyloinformatics-convert helps apply traditional phyloinformatics software to alife standardized data",
     entry_points='''
