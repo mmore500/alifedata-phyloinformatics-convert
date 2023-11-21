420d82e57e99775d53f896962616c4edba741cfc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-21 18:36:48 -0500

    fixup! Add anytree input support to rosettatree

--- tests/test_RosettaTree.py
+++ tests/test_RosettaTree.py
@@ -21,6 +21,37 @@ import yarl
 import alifedata_phyloinformatics_convert as apc
 
 
+def test_anytree_simple_tree_origin_times():
+    udo = anytree.Node("Udo", id=0, edge_length=1)
+    marc = anytree.Node("Marc", parent=udo, id=1111, edge_length=1)
+    lian = anytree.Node("Lian", parent=marc, id=2222, edge_length=1)
+    dan = anytree.Node("Dan", parent=udo, id=333, edge_length=2)
+    jet = anytree.Node("Jet", parent=dan, id=444, edge_length=4)
+    jan = anytree.Node("Jan", parent=dan, id=555, edge_length=5)
+    joe = anytree.Node("Joe", parent=dan, id=666, edge_length=6)
+
+    converted_df = apc.RosettaTree(udo).as_alife
+    expected_df = pd.DataFrame(
+        {
+            "id": [0, 1111, 333, 2222, 444, 555, 666],
+            "ancestor_list": [
+                "[None]",
+                "[0]",
+                "[0]",
+                "[1111]",
+                "[333]",
+                "[333]",
+                "[333]",
+            ],
+            "edge_length": [1, 1, 2, 1, 4, 5, 6],
+            "name": ["Udo", "Marc", "Dan", "Lian", "Jet", "Jan", "Joe"],
+            "origin_time": [1, 2, 3, 3, 7, 8, 9],
+        }
+    )
+
+    assert converted_df.equals(expected_df)
+
+
 @pytest.mark.parametrize(
     "original_df",
     [
