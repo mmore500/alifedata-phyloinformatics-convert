61659fdbb41137e099ccd04e0650d59eceecb204
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-02-13 19:13:31 -0500

    Bump version: 0.19.1 → 0.19.2

--- alifedata_phyloinformatics_convert/__init__.py
+++ alifedata_phyloinformatics_convert/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = 'm.more500@gmail.com'
-__version__ = '0.19.1'
+__version__ = '0.19.2'
 
 from .alife_dataframe_to_biopython_tree \
     import alife_dataframe_to_biopython_tree
--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/alifedata-phyloinformatics-convert',
-    version='0.19.1',
+    version='0.19.2',
     zip_safe=False,
 )
